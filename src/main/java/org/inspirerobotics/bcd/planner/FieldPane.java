955b1b94fb90b50556c69558a56f4b2c7c32930c
DevOrc <ncharlton002@gmail.com>
2019-12-21 10:06:06 -0500

    Add controls to field pane
    1. Scroll will zoom in and out
    2. WASD translates

--- src/main/java/org/inspirerobotics/bcd/planner/FieldPane.java
+++ src/main/java/org/inspirerobotics/bcd/planner/FieldPane.java
@@ -2,6 +2,9 @@ package org.inspirerobotics.bcd.planner;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 
@@ -10,16 +13,49 @@ import javafx.scene.paint.Color;
  */
 public class FieldPane extends Canvas{
 
+
     public FieldPane() {
         draw();
 
+        //Allows key events to trigger while not typing
+        this.setFocusTraversable(true);
+
         this.widthProperty().addListener(e -> draw());
         this.heightProperty().addListener(e -> draw());
+
+        this.setOnScroll(this::onScroll);
+        this.setOnKeyPressed(this::onKeyPressed);
+    }
+
+    private void onKeyPressed(KeyEvent keyEvent) {
+        if(keyEvent.getCode() == KeyCode.A){
+            getGraphicsContext2D().translate(20, 0);
+        }else if(keyEvent.getCode() == KeyCode.D){
+            getGraphicsContext2D().translate(-20, 0);
+        }
+
+        if(keyEvent.getCode() == KeyCode.W){
+            getGraphicsContext2D().translate(0, 20);
+        }else if(keyEvent.getCode() == KeyCode.S){
+            getGraphicsContext2D().translate(0, -20);
+        }
+
+        draw();
+    }
+
+    private void onScroll(ScrollEvent event) {
+        if(event.getDeltaY() > 0){
+            getGraphicsContext2D().scale(1.1, 1.1);
+        }else if(event.getDeltaY() < 0){
+            getGraphicsContext2D().scale(.9, .9);
+        }
+
+        draw();
     }
 
     private void draw(){
         GraphicsContext g = getGraphicsContext2D();
-        g.clearRect(0, 0, getWidth(), getHeight());
+        g.clearRect(-10000, -10000, 20000, 200000);
 
         g.setFill(Color.GREEN);
         g.fillRoundRect(5, 5, getWidth() - 10, getHeight() - 10, 10, 10);
